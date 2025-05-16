a30e7b79bdefc3a89b2c0683e4bb7ab546c50908
sanjayjploft <sanjayxdjploft@gmail.com>
2025-05-16 15:26:13 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+// import Layout from "./components/Layout";
 import AuthLayout from "./components/Authlayout";
 import Login from "./pages/Login";
 import Weeks from "./pages/Weeks";
